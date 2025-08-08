f7769482764edec34ecbcf1449f7c9955414c625
Will <will.mccallion@icloud.com>
2025-08-08 12:41:02 -0600

    Removed unneeded examples.

--- examples/program.s
+++
@@ -1,5 +0,0 @@
-# This is a test comment
-addi t1, x0, 10
-addi t2, zero, 20
-add s0, t1, t2
-ecall
--- examples/reg.s
+++
@@ -1,5 +0,0 @@
-main:
-    addi t1, zero, 5
-    addi t2, zero, -7
-    add s0, t1, t2
-    ecall
